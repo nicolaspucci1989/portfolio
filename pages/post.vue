d5a4046cdc1653c46c8fd9c089fdf938d1037514
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-09-11 19:26:36 -0300

    add post page

---
+++ pages/post.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="space-y-2 md:space-y-6">
+    <div class="card bg-base-200 shadow-xl">
+      <div class="card-body">
+        <h1 class="md:text-3xl font-extrabold">
+          Single Post
+          <div class="badge badge-secondary badge-lg">Shoes</div>
+        </h1>
+      </div>
+    </div>
+    <div class="card bg-base-200 shadow-xl">
+      <div class="card-body">
+        <figure>
+          <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+          />
+        </figure>
+        <div class="card-body">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Curabitur odio dolor, efficitur ac ipsum ac, feugiat
+            imperdiet massa. Integer ultricies ornare ultricies.
+            Pellentesque neque mi, venenatis quis sagittis eget,
+            congue id massa. Proin nec libero nulla. Proin ac est ac
+            mi accumsan ullamcorper sit amet at tellus. Nullam iaculis
+            malesuada dapibus. Nam sit amet dui porta, rutrum sem
+            vitae, gravida felis. Donec pretium ullamcorper quam quis
+            posuere. Aliquam nibh enim, tincidunt ut eros non, tempus
+            iaculis lacus. Integer consectetur mollis orci, vitae
+            imperdiet nisi porta nec. Praesent ultrices, mi vitae
+            consectetur lobortis, sem velit dapibus libero, sit amet
+            sodales ipsum ipsum at quam. Maecenas tincidunt arcu et
+            elementum sollicitudin. Curabitur nec quam eu magna varius
+            ultrices sit amet in ante.
+          </p>
+          <p>
+            Vivamus ullamcorper sem nulla, id vehicula lacus sagittis
+            non. Cras dignissim ligula imperdiet massa pharetra,
+            tempor ultricies magna eleifend. Donec varius mattis
+            pulvinar. Ut facilisis sapien sed risus malesuada
+            consequat. Integer ultricies tristique tincidunt. Praesent
+            semper pharetra tempor. Praesent pulvinar nibh lectus, vel
+            hendrerit lorem consectetur nec. Ut gravida magna ac
+            sodales dapibus. Aliquam at metus vitae augue auctor
+            sodales ut malesuada ex. Praesent blandit lorem ligula, ut
+            vehicula justo hendrerit id. Cras sollicitudin magna
+            tortor, in aliquam purus dignissim tincidunt. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Pellentesque
+            habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Pellentesque sit amet lacus et
+            tortor pulvinar ullamcorper.
+          </p>
+          <p>
+            Maecenas varius sapien ligula. Pellentesque tincidunt
+            mauris at venenatis viverra. Quisque fermentum ut metus
+            sed ullamcorper. In eget fermentum lectus. Nam euismod est
+            diam, quis vehicula magna luctus a. Curabitur varius,
+            massa at efficitur finibus, augue neque maximus eros, ac
+            venenatis tellus lacus non quam. Sed in neque venenatis,
+            faucibus elit vel, iaculis quam. Vivamus vel congue risus.
+            Aliquam id lectus molestie, iaculis ex eu, consequat dui.
+            Pellentesque viverra lorem eu dolor gravida, ut aliquet
+            turpis faucibus. Nullam rhoncus, nisl sed efficitur
+            faucibus, metus leo pretium mi, a consectetur elit massa
+            et metus.
+          </p>
+          <p>
+            Pellentesque at tortor porta, bibendum justo eu, sodales
+            felis. Aenean vitae mattis magna, eget semper tellus. Nam
+            mi ligula, facilisis ac molestie sed, fermentum elementum
+            justo. Nullam ullamcorper suscipit sem, eget blandit
+            ligula elementum non. Mauris a vulputate augue. Curabitur
+            viverra neque nec pretium pharetra. Maecenas non elit
+            congue, ultrices nulla tincidunt, pharetra lacus. Proin
+            gravida nisi aliquet ex vestibulum faucibus. Ut vulputate
+            in tellus sit amet vehicula. Vivamus vel nunc a orci
+            pharetra commodo.
+          </p>
+          <p>
+            Mauris tincidunt felis imperdiet, aliquam justo egestas,
+            sagittis turpis. Maecenas eu erat ex. Duis venenatis erat
+            non odio luctus finibus. Vivamus nibh libero, mattis quis
+            tempus sit amet, vestibulum ut mi. Etiam sem felis,
+            pellentesque at diam sed, sollicitudin mollis erat. Sed
+            vel ante condimentum, maximus nulla vel, congue orci. Nam
+            malesuada varius lobortis. Etiam erat ante, bibendum at
+            congue et, aliquam vel urna. Morbi rutrum tempus nunc,
+            vitae efficitur leo tristique eget. Proin tincidunt, sem
+            sed vestibulum iaculis, enim magna laoreet elit, eget
+            bibendum elit metus et felis. Cras elementum iaculis
+            ligula vel aliquam.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+
+</style>
