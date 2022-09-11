5b57728186e935052d4020f940b7763fcf9a42eb
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-09-11 17:21:56 -0300

    add items list

---
+++ components/CardListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card card-side bg-base-200 shadow-xl">
+    <figure v-if="img">
+      <img
+          :src="img"
+          alt="Book"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ title }}</h2>
+      <div class="justify-start">
+       <CategoryBtn
+           v-for="tag in tags"
+           v-bind="tag"
+       />
+      </div>
+      <p>
+        {{ text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+defineProps<{
+  title: string
+  text: string
+  img?: string
+  tags: {
+    color: string
+    name: string
+  }
+}>()
+</script>
---
+++ components/CategoryBtn.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+      class="btn btn-xs"
+      :class="colorClass"
+  >
+    {{ name }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+import {computed} from "#imports";
+
+const props = defineProps<{
+  name: string
+  color: string
+}>()
+
+const colors = {
+  yellow: 'btn-warning',
+  red: 'btn-warning',
+  green: 'btn-success',
+  secondary: 'btn-secondary',
+  primary: 'btn-primary',
+  accent: 'btn-accent'
+}
+
+const colorClass = computed(() => colors[props.color])
+</script>
