68fb2b7eb716f30e59b6a03a7f8d67c60d907b88
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-09-11 17:57:55 -0300

    extract social icons

---
+++ components/LinkedinIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <a
+      class="btn btn-sm btn-ghost btn-square"
+  >
+    <svg
+        class="inline-block w-4 h-4 fill-current"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+      <path
+          d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+      />
+      <rect x="2" y="9" width="4" height="12"/>
+      <circle cx="4" cy="4" r="2"/>
+    </svg>
+  </a>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+
+</style>
