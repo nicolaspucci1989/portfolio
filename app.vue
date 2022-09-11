7b19dc16e38b84874cb9801f3592f9a6ca018eb1
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-09-11 19:59:13 -0300

    add default layout

--- app.vue
+++ app.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="drawer drawer-mobile">
-    <input id="my-drawer-2" type="checkbox" class="drawer-toggle"/>
-    <div class="drawer-content flex justify-center lg:justify-start">
-      <div class="lg:w-[60vw] max-w-screen-lg lg:pt-4 lg:p-4">
-        <AppHeader/>
-        <div class="flex-1 p-3 md:py-[35]">
-          <NuxtPage/>
-          <div class="divider"></div>
-         <Footer/>
-        </div>
-      </div>
-    </div>
-    <AppDrawer/>
-  </div>
+  <NuxtLayout/>
 </template>
 
 <script setup>
