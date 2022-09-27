385111ed9a983238fc7166c5375444ad0a2e8fe2
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-09-27 00:50:06 -0300

    update footer (#8)

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <p class="text-sm pl-4">2022 Portfolio</p>
-  <p class="text-xs pl-4">
-    Sarissa theme desing by
-    <a class="link" href="https://github.com/iozcelik">iozcelik</a>
-  </p>
+<!--  <p class="text-xs pl-4">-->
+<!--    Sarissa theme desing by-->
+<!--    <a class="link" href="https://github.com/iozcelik">iozcelik</a>-->
+<!--  </p>-->
 </template>
 
 <script lang="ts" setup></script>
