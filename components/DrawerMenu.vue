9dc9de7bcc91b5750f17bfafd8fb6c1f3ead8f51
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-09-26 23:25:59 -0300

    comment links

--- components/DrawerMenu.vue
+++ components/DrawerMenu.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="pt-4">
     <ul class="menu bg-base-100 rounded-box">
-      <li>
-        <router-link :to="{name: 'index'}">
-          <HomeIcon/>
-          Home
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{name: 'about'}">
-          <PersonIcon/>
-          About
-        </router-link>
-      </li>
-      <li>
-        <a href="./categories.html">
-          <FileIcon/>
-          Categories
-        </a>
-      </li>
-      <li>
-        <a href="./search.html">
-          <LensIcon/>
-          Search
-        </a>
-      </li>
-      <li>
-        <a href="./archive.html">
-          <ArchiveIcon/>
-          Archive
-        </a>
-      </li>
+<!--      <li>-->
+<!--        <router-link :to="{name: 'index'}">-->
+<!--          <HomeIcon/>-->
+<!--          Home-->
+<!--        </router-link>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <router-link :to="{name: 'about'}">-->
+<!--          <PersonIcon/>-->
+<!--          About-->
+<!--        </router-link>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <a href="./categories.html">-->
+<!--          <FileIcon/>-->
+<!--          Categories-->
+<!--        </a>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <a href="./search.html">-->
+<!--          <LensIcon/>-->
+<!--          Search-->
+<!--        </a>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <a href="./archive.html">-->
+<!--          <ArchiveIcon/>-->
+<!--          Archive-->
+<!--        </a>-->
+<!--      </li>-->
       <li class="hidden lg:block">
         <div class="flex gap-1 justify-center">
           <AppSolarMode/>
@@ -44,7 +44,3 @@
 
 <script lang="ts" setup>
 </script>
-
-<style scoped>
-
-</style>
